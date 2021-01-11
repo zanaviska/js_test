bdc94af23ac0b177a9150c30707f66e5a34cbc39
zanaviska <zanaviska@users.noreply.github.com>
2021-01-11 19:50:31 +0000

    Prettified Code!

--- a.js
+++ a.js
@@ -1,11 +1,8 @@
-'use strict';
+"use strict";
 let x = 2;
-if(1 == x)
-  console.log(true);
-else
-  console.log(false)
+if (1 == x) console.log(true);
+else console.log(false);
 
-if(x == 3)
-  console.log(4);
+if (x == 3) console.log(4);
 
 console.log([1, 2, 3, 4, 5]);
